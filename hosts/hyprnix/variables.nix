2e2b5c64986c998a36fe8cd09777bc375480f933
installer <installer@gmail.com>
2024-09-27 21:31:15 -0400

    first

---
+++ hosts/hyprnix/variables.nix
@@ -0,0 +1,16 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "stein-rip";
+  gitEmail = "stein@stein.rip";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
