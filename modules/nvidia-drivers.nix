febe6f46e04cb9b4deaf93a6f4a6edef64bd2a08
stein-rip <stein@stein.rip>
2024-09-28 00:17:32 -0400

    welp

--- modules/nvidia-drivers.nix
+++ modules/nvidia-drivers.nix
@@ -17,7 +17,7 @@ in
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
       # Modesetting is required.
-      modesetting.enable = true;
+      zmodesetting.enable = true;
       # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
       powerManagement.enable = false;
       # Fine-grained power management. Turns off GPU when not in use.
